cc808704b45a9229506c7377a6afa7decef37383
notThatCode <45469340+notThatCode@users.noreply.github.com>
2025-10-03 17:20:49 -0400

    worked all day, then made this nav bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
+import { NavBar } from "../components/NavBar";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -9,12 +10,10 @@ export const Home = () => {
         {/* Background Effects */}
             <StarBackground />
         {/* Navbar */}
-
+            <NavBar />
         {/* Main Content*/}
 
         {/* Footer */}
-
-        Home
 
     </div>
 };
