185ebff845e01058b607a68558a1e87dd6b577d8
notThatCode <45469340+notThatCode@users.noreply.github.com>
2025-10-03 10:02:25 -0400

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+        {/* Theme Toggle (LAST - Nothing to test on)*/}
+            <ThemeToggle />
+        {/* Background Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content*/}
+
+        {/* Footer */}
+
+        Home
+
+    </div>
+}
